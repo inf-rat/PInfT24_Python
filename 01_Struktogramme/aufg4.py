44685cefa33a0852396a991b5adbe9a1c6e6e495
inf-rat <r.ratke@schule.bremen.de>
2024-09-02 08:16:29 +0200

    AB 01

--- 01_Struktogramme/aufg4.py
+++ 01_Struktogramme/aufg4.py
@@ -1,2 +1,22 @@
 # Aufg. 4a)
 print("Hello, World!")
+
+# Aufg. 4b)
+s = 0
+for i in range(1, 101):
+    s += i
+print(s)
+
+# Aufg. 4c)
+i = 1
+while i <= 100:
+    print(i)
+    if i == 39:
+        i = 61
+    else:
+        i += 1
+
+# Aufg. 4d)
+n = 10
+for i in range(1, n+1):
+    print(i**2)
